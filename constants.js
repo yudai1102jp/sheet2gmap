bbabe42df4ecb87399374a916d56e00091b48537
yudai1102jp <66904482+yudai1102jp@users.noreply.github.com>
2023-01-09 11:44:17 +0000

    setup

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
-const API_KEY = 'API キーを入力してください';
-const SHEET_ID = 'Google スプレッドシートの Sheet ID を入力してください';
+const API_KEY = 'AIzaSyDnLuz5oLXwZJIgz5_Y_wjLfo2qovu6Cgs';
+const SHEET_ID = '1Oy2P-gkLRTKjH3OnokVCq2t33VpWjkZmwqu_NOTbhTw';
